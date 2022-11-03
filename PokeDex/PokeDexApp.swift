fa469a608bfa75564a35b3ceba19c5505bbc0d5e
dazy1030 <odajima_naoki@cyberagent.co.jp>
2022-11-03 15:25:23 +0900

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by 小田島 直樹 on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
